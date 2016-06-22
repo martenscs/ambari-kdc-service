f3c3fcf36e253efed29808164803f76cd9fd8b76
Chris <Chris@monster>
2016-06-22 05:45:56 -0500

    Removed kdc-server config

--- package/scripts/params.py
+++ package/scripts/params.py
@@ -7,11 +7,4 @@ config = Script.get_config()
 
 kdc_realm = config['configurations']['krb5-config']['kdc.realm']
 kdc_domain = config['configurations']['krb5-config']['kdc.domain']
-#kdc_admin = config['configurations']['krb5-config']['kdc.admin']
-#kdc_adminpassword = config['configurations']['krb5-config']['kdc.adminpassword']
-#kdb_password = config['configurations']['krb5-config']['kdb.password']
-
 kdc_host = config['configurations']['krb5-config']['kdc.host']
-#detect hostname user selected for KDC
-#clusterHostInfo = config['clusterHostInfo']
-#kdc_host = str(clusterHostInfo['krb5_master_hosts'][0])
